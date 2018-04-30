7558515e5b427e6d9bbf4fc41ceea38521100c7f
Nikolay Kostov <admin@nikolay.it>
2018-04-30 15:12:06 +0300

    Introduced EnvironmentInformationProvider which provides the bot version. Added bot version to EvalHelpCommand

--- src/TcecEvaluationBot.ConsoleUI/Commands/EvalHelpCommand.cs
+++ src/TcecEvaluationBot.ConsoleUI/Commands/EvalHelpCommand.cs
@@ -2,22 +2,27 @@
 {
     using System.Linq;
 
+    using TcecEvaluationBot.ConsoleUI.Services;
     using TcecEvaluationBot.ConsoleUI.Settings;
 
     using TwitchLib.Client;
 
     public class EvalHelpCommand : BaseCommand
     {
+        private readonly EnvironmentInformationProvider environmentInformationProvider;
+
         public EvalHelpCommand(TwitchClient twitchClient, Options options, Settings settings)
             : base(twitchClient, options, settings)
         {
+            this.environmentInformationProvider = new EnvironmentInformationProvider();
         }
 
         public override string Execute(string message)
         {
             return $@"commands: 
 !eval {{engine({this.Settings.Engines.FirstOrDefault()?.Names.FirstOrDefault()})}} {{time({this.Options.DefaultEvaluationTime})}} • 
-!db • !static • !time {{#|last|next|reverse}} • !games {{engine}} • !reverse • !rand [min] [max] • evalhelp • evalengines";
+!db • !static • !time {{#|last|next|reverse}} • !games {{engine}} • !reverse • !rand [min] [max] • evalhelp • evalengines •
+<eval_bot {this.environmentInformationProvider.VersionNumber}>";
         }
     }
 }
---
+++ src/TcecEvaluationBot.ConsoleUI/Services/EnvironmentInformationProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace TcecEvaluationBot.ConsoleUI.Services
+{
+    using System;
+    using System.Reflection;
+
+    public class EnvironmentInformationProvider
+    {
+        public string VersionNumber
+        {
+            get
+            {
+                var runtimeVersion = typeof(Program).GetTypeInfo().Assembly
+                    .GetCustomAttribute<AssemblyFileVersionAttribute>();
+                var version = new Version(runtimeVersion.Version);
+                return $"{version.Major}.{version.Minor}";
+            }
+        }
+    }
+}
